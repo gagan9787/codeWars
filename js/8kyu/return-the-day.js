39f3dbfca861efb6f17371f0e5de10c424821c05
gagan9787 <gagan.courselelo@gmail.com>
2026-06-20 15:03:29 +0530

    return-the-day

---
+++ js/8kyu/return-the-day.js
@@ -0,0 +1,36 @@
+// Complete the function which returns the weekday according to the input number:
+
+// 1 returns "Sunday"
+// 2 returns "Monday"
+// 3 returns "Tuesday"
+// 4 returns "Wednesday"
+// 5 returns "Thursday"
+// 6 returns "Friday"
+// 7 returns "Saturday"
+// Otherwise returns "Wrong, please enter a number between 1 and 7"
+
+
+
+const days = {
+  1: "Sunday",
+  2: "Monday",
+  3: "Tuesday",
+  4: "Wednesday",
+  5: "Thursday",
+  6: "Friday",
+  7: "Saturday"
+}
+
+function whatday(num) { 
+  if (num <= 7 && num != 0) {
+      return days[num]
+  }
+  else{
+    return "Wrong, please enter a number between 1 and 7";
+  }
+}
+
+
+console.log(whatday(0));
+console.log(whatday(1));
+console.log(whatday(10));
